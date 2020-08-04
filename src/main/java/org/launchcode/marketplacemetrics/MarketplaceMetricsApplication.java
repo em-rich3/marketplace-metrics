2f6d7455213198610fe822994ec8288632e542ea
Emily Richardson <richardson96@gmail.com>
2020-08-03 23:03:09 -0500

    project set up with hello spring page

---
+++ src/main/java/org/launchcode/marketplacemetrics/MarketplaceMetricsApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.marketplacemetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/marketplacemetrics/MarketplaceMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.marketplacemetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
