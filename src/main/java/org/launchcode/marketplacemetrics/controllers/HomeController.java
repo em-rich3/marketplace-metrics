55169fc17b67e0390e9aeb662ffcf33b0e93dd14
Emily Richardson <richardson96@gmail.com>
2020-08-17 22:51:32 -0500

    refactored HelloController to HomeController

--- src/main/java/org/launchcode/marketplacemetrics/controllers/HomeController.java
+++ src/main/java/org/launchcode/marketplacemetrics/controllers/HomeController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloController {
+public class HomeController {
 
     @GetMapping
     @ResponseBody
-    public String hello() {
-        return "Hello, Spring";
+    public String home() {
+        return "Welcome to the home page";
     }
 
 }
